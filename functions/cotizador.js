4a1a23e66de24ae0ba579bacfc9ddb3bd28bc0d9
Morimx <82421851+Morimx@users.noreply.github.com>
2021-07-21 22:33:10 -0300

    Cotizador

--- functions/cotizador.js
+++ functions/cotizador.js
@@ -1,36 +1,76 @@
 let tecnicos = [{
-    nivel: "Nivel 1",
-    precio: "1000",
-    cantidad: [1, 2, 3, 4, 5],
-}, {
-    nivel: "Nivel 2",
-    precio: 2500,
-    cantidad: [1, 2, 3, 4],
-}, {
-    nivel: "Nivel 3",
-    precio: 4500,
-    cantidad: [1, 2, 3],
-}, ]
+        nivel: "Nivel 1",
+        precio: 1000,
+        cantidad: [1, 2, 3, 4, 5],
+        subtotal: 0,
+        clase: "nvl1",
+    }, {
+        nivel: "Nivel 2",
+        precio: 2500,
+        cantidad: [1, 2, 3, 4],
+        subtotal: 1,
+        clase: "nvl2",
+    }, {
+        nivel: "Nivel 3",
+        precio: 4500,
+        cantidad: [1, 2, 3],
+        subtotal: 2,
+        clase: "nvl3",
+    },
+
+
+
+]
+
 
 
 let acomodador = document.querySelector('#Tabla')
 
-const crearOpcion = opcion => {
-    return `<option value="${opcion}">${opcion}</option>\n`
-}
+
+const crearOpcion = opcion => { return `<option class="${opcion}">${opcion}</option>\n` }
 
 function acomodar() {
+    var y = 0;
     tecnicos.forEach(x => {
         acomodador.innerHTML = acomodador.innerHTML + `<tr>
-       <th scope="row">${x.nivel}</th>
-       <td id="n1">${x.precio}</td>
-       <td><select>
-           <option></option>
+    <th scope="row">${x.nivel}</th>
+    <td id="n1">${x.precio}</td>
+    <td>
+    <select id="${x.clase}" onchange="myFunction(${y})">
+    <option></option>
         ${x.cantidad.map(crearOpcion).join('')}
-       </select></td>
-       <td></td>
-       </tr>`
+    </select></td>
+    <td class="${x.clase}">
+    0
+    </td>
+    </tr>`
+        y++
+
     })
+
+
 }
 
 acomodar();
+
+
+function myFunction(parametro) {
+    var subtotal = parametro;
+    var change = document.getElementById("nvl1").value;
+    var change1 = document.getElementById("nvl2").value;
+    var change2 = document.getElementById("nvl3").value;
+
+    if (subtotal == 0) {
+        let preTecnico = tecnicos[0].precio
+        preTecnico = preTecnico * change;
+        document.querySelector('.nvl1').innerHTML = `${preTecnico} por hora`;
+    } else if (subtotal == 1) {
+        let preTecnico1 = tecnicos[1].precio
+        preTecnico1 = preTecnico1 * change1;
+        document.querySelector('.nvl2').innerHTML = `${preTecnico1} por hora`
+    } else if (subtotal == 2) {
+        let preTecnico2 = tecnicos[2].precio
+        preTecnico2 = preTecnico2 * change2;
+        document.querySelector('.nvl3').innerHTML = `${preTecnico2} por hora`
+    }
+}
